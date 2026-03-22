3601ed77707b84fd7d85bebc1c447d57710c8b44
jerryseo0220-droid <jerryseo0220@gmail.com>
2026-03-23 01:31:26 +0900

    프로젝트 2

---
+++ Assets/player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class player : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public float moveSpeed = 7f;
+    
+    public void OnMove(InputValue value)
+    {
+      moveInput = value.Get<Vector2>();  
+    }
+
+  
+    void Update()
+    {
+       if(moveInput.x > 0) 
+       {
+           transform.localScale = new Vector3(1,1,1);
+       }
+       else if(moveInput.x < 0 )
+       { 
+        transform.localScale = new Vector3(-1,1,1);
+       }
+       transform.Translate(new Vector3(moveInput.x,0,0) * moveSpeed * Time.deltaTime);
+    }
+   }
+  
